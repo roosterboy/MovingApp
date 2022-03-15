a49874a1ca22ccd5482ce398f461762bd772c1c4
agholson <87455899+agholson@users.noreply.github.com>
2022-03-14 21:05:09 -0400

    Initial Commit

---
+++ MovingApp/MovingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovingAppApp.swift
+//  MovingApp
+//
+//  Created by Leone on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct MovingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
